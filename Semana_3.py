b0ac3c8d4344222bfbd90027211734ffd23e07cf
FrancoCattarozzi <franco.cattarozzi@gmail.com>
2025-05-13 19:54:53 -0300

    Primer commit al repositorio

---
+++ Semana_3.py
@@ -0,0 +1,24 @@
+# lista vacia
+# lista_vacia = []
+# print(lista_vacia)
+# print(type(lista_vacia))
+
+# lista_vacia = list()
+# print(lista_vacia)
+# print(type(lista_vacia))
+
+lista = [1, "Hola mundo", 1.5, "1", ["otra", "lista"], True, {}]
+# print(lista)
+
+print(lista[2])
+# agrega un elemento al final
+lista.append("nuevo")
+print(lista)
+
+# agregar un numero en el indice número 3
+lista.insert(3, "Adios")
+print(lista)
+
+# eleminar el ultimo elemento por defecto sino ingresar el índice
+lista.pop(3)
+print(lista)
